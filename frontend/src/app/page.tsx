5696be02feb467fae67d48461377720db5d24299
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2025-04-14 16:02:13 -0600

    added welcome

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,22 +1,73 @@
 "use client";
 
-import { useMutation } from "@tanstack/react-query";
-import { callBackend } from "../lib/api/test";
-import styles from "./page.module.css";
+import Image from "next/image";
 
-export default function Home() {
-  const { mutate, data, isPending, error } = useMutation({
-    mutationFn: callBackend,
-  });
-
-  if (isPending) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
-
+const Welcome = () => {
   return (
-    <div className={styles.page}>
-      <p>Hello World.</p>
-      <button onClick={() => mutate()}>Call Backend</button>
-      <p>{JSON.stringify(data)}</p>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+        width: "100%",
+        maxWidth: "800px",
+        margin: "0px auto",
+        padding: "20px",
+        boxSizing: "border-box",
+      }}
+    >
+      <div style={{ maxWidth: "450px" }}>
+        <h1>Welcome to Photo Palettes!</h1>
+        <p>
+          A social platform for generating color palettes from photos inspired
+          by{" "}
+          <a target="_blank" href="https://seis.pointlessprojects.com">
+            Seis Colores / Six Colors
+          </a>
+          . Check out some of the examples below.
+        </p>
+        <h2>Inspired?</h2>
+        <p>
+          <a target="_blank" href="https://forms.gle/DZv1LyEwmEMmZPcC8">
+            Join the Closed Alpha
+          </a>
+          !
+        </p>
+        <p>
+          <a
+            target="_blank"
+            href="https://bsky.app/profile/sillysideprojects.bsky.social"
+          >
+            Build with Me
+          </a>
+          !
+        </p>
+        <p>
+          - Travis Bumgarner (
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/travisbumgarner/"
+          >
+            LinkedIn
+          </a>
+          ,&nbsp;
+          <a target="_blank" href="https://travisbumgarner.dev">
+            Portfolio
+          </a>
+          )
+        </p>
+      </div>
+      <div style={{ width: "100%", aspectRatio: 16 / 9, position: "relative" }}>
+        <Image fill alt="color palette 1" src="/landing_page/1.png" />
+      </div>
+      <div style={{ width: "100%", aspectRatio: 16 / 9, position: "relative" }}>
+        <Image fill alt="color palette 1" src="/landing_page/2.png" />
+      </div>
+      <div style={{ width: "100%", aspectRatio: 16 / 9, position: "relative" }}>
+        <Image fill alt="color palette 1" src="/landing_page/3.png" />
+      </div>
     </div>
   );
-}
+};
+
+export default Welcome;
