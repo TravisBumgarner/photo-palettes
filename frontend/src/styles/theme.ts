d28770d7369134cec8c22fc9ea825b119aca7d0b
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2025-04-19 22:55:07 -0600

    fix some docker stuff

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -4,4 +4,13 @@ import { createTheme } from '@mui/material'
 
 export const theme = createTheme({
   cssVariables: true,
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#000000',
+    },
+    secondary: {
+      main: '#000000',
+    },
+  },
 })
