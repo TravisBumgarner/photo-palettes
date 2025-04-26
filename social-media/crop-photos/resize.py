300ee1ccf062e7aa6d61b478094c68fbc40b9f53
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2025-04-25 19:02:45 -0600

    done

--- social-media/crop-photos/resize.py
+++ social-media/crop-photos/resize.py
@@ -1,9 +1,16 @@
 import os
 import shutil
-from PIL import Image, UnidentifiedImageError, ImageChops
+
+from PIL import Image, ImageChops, UnidentifiedImageError
 
 IMG_EXTS = {".png", ".jpg", ".jpeg", ".bmp", ".gif", ".tiff"}
 CROP = 300
+
+
+def crop_top_row(img):
+    # Apple is adding 1 row of black pixels to the top of the image. Bug?
+    w, h = img.size
+    return img.crop((0, 1, w, h))
 
 
 def crop_white(img):
@@ -33,29 +40,55 @@ def crop_200(img):
     return img.crop((CROP, CROP, w - CROP, h - CROP))
 
 
+# starting index of seis colores
+
+
 def process_images():
+    counter = 36
     outdir = "output"
     indir = "input"
     if os.path.exists(outdir):
         shutil.rmtree(outdir)
     os.makedirs(outdir)
     if not os.path.exists(indir):
+<<<<<<< Updated upstream
         print(f"Input directory '{indir}' does not exist.")
+=======
+        print(f"Input directory '{indir}' does not exist")
+>>>>>>> Stashed changes
         return
+
+    print(f"Looking for images in '{indir}'")
+
+    # Get all image files and sort them by filename (which contains date)
+    image_files = []
     for fname in os.listdir(indir):
         base, ext = os.path.splitext(fname)
         if ext.lower() not in IMG_EXTS:
             continue
+        print(f"Found image: {fname}")
+        image_files.append(fname)
+
+    # Sort filenames (which will sort by date since they contain date)
+    image_files.sort()
+
+    print(f"Found {len(image_files)} images")
+
+    # Process sorted files
+    for fname in image_files:
         fpath = os.path.join(indir, fname)
         try:
             img = Image.open(fpath).convert("RGB")
         except (UnidentifiedImageError, OSError):
+            print(f"Error opening {fpath}")
             continue
+        img = crop_top_row(img)  # Crop 1 row of pixels from the top
         img = crop_white(img)
         img = pad_to_square_with_min_padding(img, min_pad=30)
-        outname = os.path.join(outdir, fname)
+        outname = os.path.join(outdir, f"{counter:03d}_{fname}")
         img.save(outname)
         print(f"Saved {outname}")
+        counter += 1
 
 
 if __name__ == "__main__":
