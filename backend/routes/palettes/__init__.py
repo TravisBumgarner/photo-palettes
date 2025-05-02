da2df42254cb4f2229b7ab3578a9fb18c30559c9
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2025-05-01 20:29:37 -0600

    cleanup

--- backend/routes/palettes/__init__.py
+++ backend/routes/palettes/__init__.py
@@ -6,7 +6,6 @@ palettes_router = APIRouter()
 from . import (
     create_palette,
     generate_palette,
-    get_og_url,
     get_palette_by_id,
     get_palette_list_as_moderator,
     get_palette_list_by_app_user_id,
--- backend/routes/palettes/get_og_url.py
+++
@@ -1,6 +0,0 @@
-from . import palettes_router
-
-
-@palettes_router.get("/og_url/{id}")
-async def get_og_url(id: str):
-    return f"http://localhost:8000/uploads/{id}_og.webp"
