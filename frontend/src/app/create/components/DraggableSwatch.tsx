2d98b4b81761e1b1c17ca12ea57f7ffb9954eb26
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2025-05-09 14:38:11 -0600

    progress

--- frontend/src/app/create/components/DraggableSwatch.tsx
+++ frontend/src/app/create/components/DraggableSwatch.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion'
-import { forwardRef } from 'react'
+import { forwardRef, useCallback, useState } from 'react'
 
 const DraggableSwatch = forwardRef<
   HTMLDivElement,
@@ -8,23 +8,36 @@ const DraggableSwatch = forwardRef<
     isDragging: boolean
   }
 >(({ isHovering, isDragging }, ref) => {
+  const [isHoveringInternally, setIsHoveringInternally] = useState(false)
+
+  const handleMouseEnter = useCallback(() => {
+    setIsHoveringInternally(true)
+  }, [])
+
+  const handleMouseLeave = useCallback(() => {
+    setIsHoveringInternally(false)
+  }, [])
+
   return (
     <motion.div
       ref={ref}
       initial={{
-        scale: isHovering || isDragging ? 3 : 1,
+        scale: isHovering || isHoveringInternally || isDragging ? 3 : 1,
       }}
       animate={{
-        scale: isHovering || isDragging ? 3 : 1,
+        scale: isHovering || isHoveringInternally || isDragging ? 3 : 1,
       }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
       style={{
         // Background and position are set by refs from the parent.
         position: 'absolute',
         transform: 'translate(-50%, -50%)',
         width: '20px',
+        cursor: 'grab',
         height: '20px',
         borderRadius: '50%',
-        border: isDragging ? 'none' : `2px solid black`,
+        border: `2px solid black`,
         boxShadow: '0 0 10px rgba(0,0,0,0.3)',
       }}
     />
