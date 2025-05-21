285cd8ef6745f6f7cf91527ecf74308d129a0bb7
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2025-05-21 16:41:37 -0600

    cleaned up tos

--- frontend/src/app/tos/page.tsx
+++ frontend/src/app/tos/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { List, ListItem, ListItemText, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 import { PageTitle, PageWrapper } from '../../styles/Shared'
 import Link from '../_sharedComponents/Link'
 const TermsOfService = () => {
@@ -8,51 +8,39 @@ const TermsOfService = () => {
     <PageWrapper width="medium" staticContent>
       <PageTitle text="Terms of Service" />
       <Typography variant="body1">
-        <em>Last updated: April 17, 2025</em>
+        <em>Last updated: May 21, 2025</em>
       </Typography>
 
       <Typography variant="h3">1. Use of the Service</Typography>
       <Typography variant="body1">
-        You may use Photo Palettes to upload images and generate color palettes for personal or
+        You may use Photo Palettes to upload photos and generate color palettes for personal or
         commercial use. You are responsible for ensuring that any content you upload complies with
         applicable laws and that you have the rights to use and upload that content.
       </Typography>
 
       <Typography variant="h3">2. User Content & Sharing</Typography>
       <Typography variant="body1">
-        You retain all rights to the images you upload. By uploading content, you grant us a
+        You retain all rights to the photos you upload. By uploading content, you grant us a
         limited, non-exclusive, worldwide, royalty-free license to display, distribute, and share
         your content within the Photo Palettes platform and on third-party platforms for the purpose
         of enabling discovery, sharing, and social features.
       </Typography>
       <Typography variant="body1">
         Photo Palettes is a social platform. This means content you upload will be visible to other
-        users, who can like, share, or comment on it. Public images and their associated palettes
+        users, who can like, share, or comment on it. Public photos and their associated palettes
         may be featured on the site or shared around the internet via social feeds, newsletters, or
         integrations with third-party platforms.
       </Typography>
 
       <Typography variant="h3">3. Restrictions</Typography>
       <Typography variant="body1">You agree not to use the Service to:</Typography>
-      <List>
-        <ListItem>
-          <ListItemText>Upload illegal, obscene, or infringing content.</ListItemText>
-        </ListItem>
-        <ListItem>
-          <ListItemText>Upload content that is not your own.</ListItemText>
-        </ListItem>
-        <ListItem>
-          <ListItemText>Upload AI-generated content.</ListItemText>
-        </ListItem>
-        <ListItem>
-          <ListItemText>Interfere with or disrupt the service.</ListItemText>
-        </ListItem>
-        <ListItem>
-          <ListItemText>
-            Attempt to reverse engineer or misuse any part of the platform.
-          </ListItemText>
-        </ListItem>
-      </List>
+      <ul>
+        <li>Upload illegal, obscene, or infringing content.</li>
+        <li>Upload content that is not your own.</li>
+        <li>Upload AI-generated content.</li>
+        <li>Interfere with or disrupt the service.</li>
+        <li>Attempt to reverse engineer or misuse any part of the platform.</li>
+      </ul>
 
       <Typography variant="h3">4. Intellectual Property</Typography>
       <Typography variant="body1">
@@ -79,13 +67,13 @@ const TermsOfService = () => {
         damages resulting from your use of the Service.
       </Typography>
 
-      <Typography variant="h3">8. Changes</Typography>
+      {/* <Typography variant="h3">8. Changes</Typography>
       <Typography variant="body1">
         We may update these Terms from time to time. If we make material changes, we’ll notify you
         through the Service or via email.
-      </Typography>
+      </Typography> */}
 
-      <Typography variant="h3">9. Contact</Typography>
+      <Typography variant="h3">8. Contact</Typography>
       <Typography variant="body1">
         If you have questions or concerns, contact us at{' '}
         <Link href="mailto:support@photopalettes.com">support@photopalettes.com</Link>.
