4b4c4db381ee84fc1a63b5054602cf0d0ea9538e
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2025-08-21 13:31:02 -0600

    capacitor

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.photopalettes',
+  appName: 'Photo Palettes',
+  webDir: 'dist'
+};
+
+export default config;
