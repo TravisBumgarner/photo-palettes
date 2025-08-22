e09718ee05146e914389cc2887c809d441e909ba
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2025-08-21 18:07:06 -0600

    [SSP-660] Frontend kmeans (#79)
    * nice
    
    * POC looks good

---
+++ explorations/frontend_kmeans/src/kmeans.ts
@@ -0,0 +1,18 @@
+function samplePixels(data: number[][], sampleSize = 250_000) {
+  if (data.length <= sampleSize) return data;
+  const sampled: number[][] = [];
+  for (let i = 0; i < sampleSize; i++) {
+    sampled.push(data[Math.floor(Math.random() * data.length)]);
+  }
+  return sampled;
+}
+
+import skmeans from "skmeans";
+
+const kmeans = (data: number[][], k: number, sampleSize = 250_000) => {
+  const sampled = samplePixels(data, sampleSize);
+  const result = skmeans(sampled, k);
+  return result.centroids;
+};
+
+export default kmeans;
