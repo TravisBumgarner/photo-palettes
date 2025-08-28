86ecebc7d7cdc04400135dc1d94e034198cfdf9c
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2025-08-28 11:07:05 -0600

    cleanup

--- backend/routes/favorites/remove_from_favorites.py
+++ backend/routes/favorites/remove_from_favorites.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 from consts import ERROR_MSG
 from database.queries.favorites import remove_palette_from_favorites
 from middleware.auth import RequestWithAuthState
-from routes.shared import AuthedRequest, InvalidRequest
+from routes.shared import AuthedRequest, BaseErrorResponse, BaseSuccessResponse, InvalidRequest
 from services.logger import log_error
 from utils.auth import user_is_authed
 
@@ -16,11 +16,6 @@ ROUTE_NAME = "remove_from_favorites"
 
 class Body(BaseModel):
     palette_id: uuid.UUID
-
-
-class Response(BaseModel):
-    success: bool
-    message: str | None = None
 
 
 def parse_request(raw_request: RequestWithAuthState) -> AuthedRequest | InvalidRequest:
@@ -40,11 +35,15 @@ async def remove_to_favorites(raw_request: RequestWithAuthState, body: Body):
         match parsed_request:
             case InvalidRequest(error=error):
                 log_error(RuntimeError(error), ROUTE_NAME)
-                return Response(success=False, message=error)
+                return BaseErrorResponse(message=error)
             case AuthedRequest(app_user_id=app_user_id):
                 result = remove_palette_from_favorites(app_user_id, body.palette_id)
-                return Response(success=result)
+                if result:
+                    return BaseSuccessResponse(True)
+                else:
+                    log_error(RuntimeError("Failed to remove palette from favorites"), ROUTE_NAME)
+                    return BaseErrorResponse(message=ERROR_MSG.SOMETHING_WENT_WRONG)
 
     except Exception as e:
         log_error(e, ROUTE_NAME)
-        return Response(success=False, message=ERROR_MSG.SOMETHING_WENT_WRONG)
+        return BaseErrorResponse(message=ERROR_MSG.SOMETHING_WENT_WRONG)
