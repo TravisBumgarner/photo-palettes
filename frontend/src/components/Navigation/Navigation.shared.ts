06faeb642b4c72ee25e6ee4ed9e82b9026079466
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2025-09-06 10:31:10 -0600

    done

--- frontend/src/components/Navigation/Navigation.shared.ts
+++ frontend/src/components/Navigation/Navigation.shared.ts
@@ -1,8 +1,10 @@
 import type { ROUTES } from '../../consts'
+import { Capacitor } from '@capacitor/core'
 
 type Items = (keyof typeof ROUTES | 'divider')[]
 
 export const USER_ROUTES: Items = [
+  ...(Capacitor.isNativePlatform() ? (['browse'] as const) : []),
   'favorites',
   'profile',
   'feedback',
@@ -15,6 +17,7 @@ export const MODERATOR_ROUTES: Items = [...USER_ROUTES, 'divider', 'moderation']
 export const ADMIN_ROUTES: Items = [...MODERATOR_ROUTES, 'divider', 'admin']
 
 export const ANON_ROUTES: Items = [
+  ...(Capacitor.isNativePlatform() ? (['browse'] as const) : []),
   'login',
   'signup',
   'feedback',
