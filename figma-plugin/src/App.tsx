afe68c575d74685192dd1b715954e129a13c22fe
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2025-09-30 12:50:03 -0600

    remove sentry

--- figma-plugin/src/App.tsx
+++ figma-plugin/src/App.tsx
@@ -6,13 +6,6 @@ import Create from "./src-app/pages/Create";
 import { SPACING } from "./src-app/styles/styleConsts";
 import * as Sentry from "@sentry/react";
 
-Sentry.init({
-  dsn: "https://0cc7281ca80d476d7240e129a28263a3@o196886.ingest.us.sentry.io/4510110002380800",
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
-  sendDefaultPii: true,
-});
-
 function App() {
   return (
     <Box
--- figma-plugin/src/src-app/services/logging.ts
+++ figma-plugin/src/src-app/services/logging.ts
@@ -1,22 +1,11 @@
-import { captureException, captureMessage } from "@sentry/react";
-
+// Sentry does not run in Figma. Would need to proxy through backend.
 export const logger = {
   info: (message: string) => {
-    if (false) {
-      // Todo fix
-      captureMessage(message);
-    } else {
-      // eslint-disable-next-line no-console
-      console.log(message);
-    }
+    // eslint-disable-next-line no-console
+    console.log(message);
   },
   error: (...args: (string | Error | unknown)[]) => {
-    if (false) {
-      // Todo fix
-      captureException(JSON.stringify(args));
-    } else {
-      // eslint-disable-next-line no-console
-      console.error(args.map((arg) => JSON.stringify(arg)).join(" "));
-    }
+    // eslint-disable-next-line no-console
+    console.error(args.map((arg) => JSON.stringify(arg)).join(" "));
   },
 };
